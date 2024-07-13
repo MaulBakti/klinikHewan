ec98787b610d34e97392155d4c29afc173487094
MohammadRadja <you@example.com>
2024-07-13 17:14:38 +0700

    revisi

--- lib/app/modules/forgotpass/controllers/forgotpass_controller.dart
+++ lib/app/modules/forgotpass/controllers/forgotpass_controller.dart
@@ -1,23 +1,32 @@
 import 'package:get/get.dart';
 
 class ForgotpassController extends GetxController {
-  //TODO: Implement ForgotpassController
+  var usernameOrTelp = ''.obs;
+  var newPassword = ''.obs;
+  var confirmPassword = ''.obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  void resetPassword() {
+    if (usernameOrTelp.value.isEmpty) {
+      Get.snackbar('Error', 'Username/Telephone is required');
+      return;
+    }
+
+    if (newPassword.value.isEmpty) {
+      Get.snackbar('Error', 'New Password is required');
+      return;
+    }
+
+    if (confirmPassword.value.isEmpty) {
+      Get.snackbar('Error', 'Confirm Password is required');
+      return;
+    }
+
+    if (newPassword.value != confirmPassword.value) {
+      Get.snackbar('Error', 'Passwords do not match');
+      return;
+    }
+
+    // Perform the password reset logic (e.g., API call)
+    Get.snackbar('Success', 'Password has been reset');
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/forgotpass/views/forgotpass_view.dart
+++ lib/app/modules/forgotpass/views/forgotpass_view.dart
@@ -1,23 +1,146 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import '../controllers/forgotpass_controller.dart';
 
 class ForgotpassView extends GetView<ForgotpassController> {
   const ForgotpassView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final RxBool _obscureTextNew = true.obs;
+    final RxBool _obscureTextConfirm = true.obs;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ForgotpassView'),
+        title: const Text('Forgot Password'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ForgotpassView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Stack(
+        children: [
+          // // Background image
+          // Container(
+          //   decoration: BoxDecoration(
+          //     image: DecorationImage(
+          //       image: AssetImage(
+          //           'assets/images/background.jpg'), // Update with your background image path
+          //       fit: BoxFit.cover,
+          //     ),
+          //   ),
+          // ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+// Logo
+                      Image.asset(
+                        'assets/images/logo.webp', // Ganti dengan path gambar logo
+                        height: 100,
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        onChanged: (value) =>
+                            controller.usernameOrTelp.value = value,
+                        decoration: InputDecoration(
+                          labelText: 'Username/Telephone',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      // New Password TextField
+                      Obx(() {
+                        return TextField(
+                          onChanged: (value) =>
+                              controller.newPassword.value = value,
+                          decoration: InputDecoration(
+                            labelText: 'New Password',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscureTextNew.value
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                _obscureTextNew.value = !_obscureTextNew.value;
+                              },
+                            ),
+                          ),
+                          obscureText: _obscureTextNew.value,
+                        );
+                      }),
+                      SizedBox(height: 15),
+                      // Confirm Password TextField
+                      Obx(() {
+                        return TextField(
+                          onChanged: (value) =>
+                              controller.confirmPassword.value = value,
+                          decoration: InputDecoration(
+                            labelText: 'Confirm Password',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscureTextConfirm.value
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                _obscureTextConfirm.value =
+                                    !_obscureTextConfirm.value;
+                              },
+                            ),
+                          ),
+                          obscureText: _obscureTextConfirm.value,
+                        );
+                      }),
+                      SizedBox(height: 15),
+                      // Submit Button
+                      ElevatedButton(
+                        onPressed: () {
+                          controller.resetPassword();
+                        },
+                        child: Text('Reset'),
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.black),
+                          foregroundColor:
+                              MaterialStateProperty.all<Color>(Colors.white),
+                          minimumSize:
+                              MaterialStateProperty.all<Size>(Size(200, 50)),
+                          shape: MaterialStateProperty.all<OutlinedBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
