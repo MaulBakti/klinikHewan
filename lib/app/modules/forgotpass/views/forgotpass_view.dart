ee3b54c631464837bf41ea1708ba8693aa71c718
MaulanaBhakti <maulana100801@gmail.com>
2024-07-14 18:48:04 +0700

    "revisi"

--- lib/app/modules/forgotpass/views/forgotpass_view.dart
+++ lib/app/modules/forgotpass/views/forgotpass_view.dart
@@ -13,10 +13,69 @@ class ForgotpassView extends GetView<ForgotpassController> {
         title: const Text('ForgotpassView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ForgotpassView is working',
-          style: TextStyle(fontSize: 20),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            elevation: 8,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 15),
+                  TextField(
+                    // onChanged: (value) => controller.noTelp.value = value,
+                    decoration: InputDecoration(
+                      labelText: 'No Telp',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Cari'),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      minimumSize:
+                          MaterialStateProperty.all<Size>(Size(200, 50)),
+                      shape: MaterialStateProperty.all<OutlinedBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {
+                      Get.toNamed('/dashboard');
+                    },
+                    child: Text('Batal'),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.grey),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                      minimumSize:
+                          MaterialStateProperty.all<Size>(Size(200, 50)),
+                      shape: MaterialStateProperty.all<OutlinedBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
